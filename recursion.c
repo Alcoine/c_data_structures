f7692658917e4ec6d408d5ed0014233ae466fbd6
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-04-26 21:19:34 -0700

    Adding a recursion to find a sum of seq of natural numbers until n.

--- recursion.c
+++ recursion.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-void fun(int n)
+int fun(int n)
 {
-    if (n > 0)
-    {
-        printf("%d ", n);
-        fun(n - 1);
-        fun(n - 1);
-    }
+    if (n > 100)
+        return n - 10;
+
+    return fun(fun(n + 11));
 }
 
-int main()
+int main ()
 {
-    fun(5);
+    int r;
+    r = fun(95);
+
+    printf("%d\n", r);
+
     return 0;
 }
