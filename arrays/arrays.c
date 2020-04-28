96762d3c420de645dadc1897cdef035e99b79231
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-04-27 22:22:16 -0700

    Modifying arrays: adding a new pointer to a bigger array than prev one.

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -3,15 +3,27 @@
 
 int main()
 {
-    int *p;
+    int *p, *q, i;
     p = (int *)malloc(5 * sizeof(*p));
+    q = (int *)malloc(5 * sizeof(*p));
 
-    for (int i = 0; i < 5; i += 1)
+    for (i = 0; i < 5; i += 1)
     {
-        printf("%d \n", p[i]);
+        p[i] = i * 100;
+    }
+
+    for (i = 0; i < 5; i += 1)
+    {
+        q[i] = p[i];
     }
 
     free(p);
+
+    p = q;
+
+    q = NULL;
+
+    printf("%d \n", p[2]);
 
     return 0;
 }
