1e928933c5683f8c8a1eab408c29292035118ba3
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-04-27 22:40:38 -0700

    Adding a function to create 2d array for 5 elem.

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int * create_2d_array();
+
 int main()
 {
-    int *p, *q, i;
+    int *p, *q, i, *f;
     p = (int *)malloc(5 * sizeof(*p));
     q = (int *)malloc(5 * sizeof(*q));
 
@@ -17,8 +19,22 @@ int main()
 
     p = q;
     q = NULL;
+    f = create_2d_array(f);
+    f[0] = 1090;
+    printf("f pointer non-free: %d \n", f[0]);
 
-    printf("%d \n", p[2]);
+    free(f);
+
+    printf("f pointer free: %d \n", f[0]);
+    printf("p pointer: %d \n", p[2]);
 
     return 0;
+}
+
+int * create_2d_array()
+{
+    int *p;
+    p = (int *)malloc(10 * sizeof(*p));
+
+    return p;
 }
