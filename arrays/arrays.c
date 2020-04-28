6813a634c9ecb28d6904996c73d305072b65a37f
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-04-27 22:23:29 -0700

    Refactoring for loops. Fixing a typo in malloc func call.

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -5,22 +5,17 @@ int main()
 {
     int *p, *q, i;
     p = (int *)malloc(5 * sizeof(*p));
-    q = (int *)malloc(5 * sizeof(*p));
+    q = (int *)malloc(5 * sizeof(*q));
 
     for (i = 0; i < 5; i += 1)
-    {
         p[i] = i * 100;
-    }
 
     for (i = 0; i < 5; i += 1)
-    {
         q[i] = p[i];
-    }
 
     free(p);
 
     p = q;
-
     q = NULL;
 
     printf("%d \n", p[2]);
