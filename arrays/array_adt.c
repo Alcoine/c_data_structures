5411e594a9d2cd4be09d8d27975b45bc0168847b
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-04-30 21:53:41 -0700

    Modifying all functions on arrays: passing a struct by a reference. Adding checks.

--- arrays/array_adt.c
+++ arrays/array_adt.c
@@ -13,17 +13,17 @@ struct Array
 };
 
 
-void display(struct Array array);
-void insert(struct Array array, int element, int atIdx);
-void add(struct Array array, int element);
-void delete(struct Array array);
-void search(struct Array array);
-void get(struct Array array);
-void set(struct Array array);
-void max(struct Array array);
-void min(struct Array array);
-void reverse(struct Array array);
-void shift(struct Array array);
+void display(struct Array *array);
+void insert(struct Array *array, int element, int atIdx);
+void add(struct Array *array, int element);
+void delete(struct Array *array);
+void search(struct Array *array);
+void get(struct Array *array);
+void set(struct Array *array);
+void max(struct Array *array);
+void min(struct Array *array);
+void reverse(struct Array *array);
+void shift(struct Array *array);
 
 int main()
 {
@@ -34,31 +34,34 @@ int main()
 
     array.arr = (int *)malloc(array.size * sizeof(array.arr));
     array.length = 0;
-    insert(array, 123, 0);
+    insert(&array, 123, 0);
     printf("%d", array.arr[0]);
 
     return 0;
 }
 
-void display(struct Array array)
+void display(struct Array *array)
 {
-    for (int i = 0; i < array.length; i += 1)
-        printf("%d, ", array.arr[i]);
+    for (int i = 0; i < (*array).length; i += 1)
+        printf("%d, ", (*array).arr[i]);
 }
 
-void insert(struct Array array, int element, int atIdx)
+void insert(struct Array *array, int element, int atIdx)
 {
     // TODO: add check of adIdx and actual array length/size
 
-    for (int i = array.length; i > atIdx; i -= 1)
-        array.arr[i] = array.arr[i - 1];
+    for (int i = (*array).length; i > atIdx; i -= 1)
+        (*array).arr[i] = (*array).arr[i - 1];
 
-    array.arr[atIdx] = element;
-    array.length += 1;
+    (*array).arr[atIdx] = element;
+    (*array).length += 1;
 }
 
-void add(struct Array array, int element)
+void add(struct Array *array, int element)
 {
-    array.arr[array.length] = element;
-    array.length += 1;
+    if ((*array).length < (*array).size)
+    {
+        (*array).arr[(*array).length] = element;
+        (*array).length += 1;
+    }
 }
