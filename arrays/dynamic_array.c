24a28df789b06b018a9ee254639e1d97a075ad22
Vladimir Ivanov <vladimir.ivanovsf@gmail.com>
2020-05-08 22:14:23 -0700

    Addint reading list.

--- arrays/dynamic_array.c
+++ arrays/dynamic_array.c
@@ -2,4 +2,7 @@ int main()
 {
     // TODO: read about calloc and malloc
     // TODO: implement dynamic array based on provided size * 2
+    // https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/
+    // https://www.geeksforgeeks.org/stack-vs-heap-memory-allocation/
+    // https://www.geeksforgeeks.org/memory-layout-of-c-program/
 }
